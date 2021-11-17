57606055d4ad4e1486c327ec714301fa48d55fe1
Pedro <pedro.trabalhos@outlook.com>
2021-11-17 15:55:01 -0300

    full spec

---
+++ index.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+     host: "outlook.office365.com",
+     port: "587",
+     secure: false,
+     auth:{
+          user: "****@hotmail.com",     
+          pass: "***"
+     }
+});
+
+transporter.sendMail({
+     from: "Pedro de Sá <sahenriquepedro@hotmail.com>",
+     to: "pedro.trabalhos@outlook.com",
+     subject: "Esse é um teste",
+     text: "Oi, eu acho que esse texto funcionou",
+}).then(message =>{
+     console.log(message);
+}).catch(err =>{
+     console.log(err);
+});
+
